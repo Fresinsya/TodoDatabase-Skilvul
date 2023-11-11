f0840a63a1951ed35c67264598f049abb9972bed
Fresinsya <vicafresinsya@gmail.com>
2023-11-11 14:04:53 +0700

    deleteById

--- controllers/Todo-controller.js
+++ controllers/Todo-controller.js
@@ -54,5 +54,98 @@ module.exports = {
                 error: error.message
             })
         }
-    }
+    },
+
+    // read todo by id
+    readTodoDetail: async (req, res) => {
+        const idTodo = req.params.id;
+        const userID = req.user.id;
+
+        try {
+            const todo = await Todos.findOne({_id: idTodo, UserId: userID})
+            if(!todo){
+                res.status(404).json({
+                    message: "Todo tidak ditemukan"
+                })
+                return;
+            }
+            res.status(200).json({
+                message: "Berhasil mendapatkan data",
+                data: todo
+            })
+
+        } catch (error) {
+            res.status(500).json({
+                message: "Gagal mendapatkan data",
+                error: error.message
+            })
+        }
+
+    },
+
+    // update todo by id
+    updateTodo: async (req, res) => {
+        const idTodo = req.params.id;
+        const userID = req.user.id;
+
+        const data = req.body;
+        const { value, status } = data;
+
+        try {
+            const todo = await Todos.findOne({_id: idTodo, UserId: userID})
+            if(!todo){
+                res.status(404).json({
+                    message: "Todo tidak ditemukan"
+                })
+                return;
+            }
+
+            todo.value = value || todo.value;
+            todo.status = status || todo.status;
+
+            await todo.save();
+
+            res.status(200).json({
+                message: "Berhasil update data",
+                data: todo
+            })
+
+        } catch (error) {
+            res.status(500).json({
+                message: "Gagal update data",
+                error: error.message
+            })
+        }
+    },
+
+    // delete todo by id
+    deleteTodo: async (req, res) => {
+        const idTodo = req.params.id;
+        const userID = req.user.id;
+
+        try {
+            const todo = await Todos.findOne({_id: idTodo, UserId: userID})
+            if(!todo){
+                res.status(404).json({
+                    message: "Todo tidak ditemukan"
+                })
+                return;
+            }
+
+            await todo.deleteOne();
+
+            res.status(200).json({
+                message: "Berhasil delete data",
+                data: todo
+            })
+
+        } catch (error) {
+            res.status(500).json({
+                message: "Gagal delete data",
+                error: error.message
+            })
+        }
+    },
+
+    
 }
--- routes/todo-routes.js
+++ routes/todo-routes.js
@@ -1,9 +1,12 @@
 const express = require('express');
-const { createTodo, readTodos } = require('../controllers/Todo-controller');
+const { createTodo, readTodos, readTodoDetail, updateTodo, deleteTodo, deleteAllTodo } = require('../controllers/Todo-controller');
 const { verifyToken } = require('../middleware/auth');
 const route = express.Router();
 
 route.post('/', verifyToken, createTodo)
 route.get('/', verifyToken, readTodos)
+route.get('/:id', verifyToken, readTodoDetail)
+route.put('/:id', verifyToken, updateTodo)
+route.delete('/:id', verifyToken, deleteTodo)
 
 module.exports = route;
